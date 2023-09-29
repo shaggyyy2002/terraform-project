eeb6f679340cbf80130a0496e742ba73fff18b6c
Nitin Gouda <nitinagouda@acpce.ac.in>
2023-09-29 11:14:25 +0000

    update

--- bin/install-aws-cli.sh
+++ bin/install-aws-cli.sh
@@ -1,18 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cd /workspace
 
+rm -f '/workspace/awscliv2.zip'
+rm -rf '/workspace/aws'
+
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-
 unzip awscliv2.zip
-
 sudo ./aws/install
-
-./aws/install -i /usr/local/aws-cli -b /usr/local/bin
-
-aws --version
-
-sudo apt update
 
 aws sts get-caller-identity
 
