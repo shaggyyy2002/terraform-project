724e323e0f71e2e810e5918660f47bae7501f00f
Nitin Gouda <nitin.gouda@fintricity.com>
2023-10-07 22:12:25 +0000

    enabled static webiste hosting

--- variables.tf
+++ variables.tf
@@ -6,3 +6,11 @@ variable "bucket_name" {
   description = "The name of the AWS S3 bucket."
   type        = string
 }
+variable "index_html_filepath" {
+  description = "Local path to the index.html file"
+  type        = string
+}
+variable "error_html_filepath" {
+  description = "Local path to the index.html file"
+  type        = string
+}
